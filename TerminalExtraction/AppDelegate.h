b7098c1525843680289284eb3f77568009c60698
Yarik Smirnov <yarikbonch@me.com>
2012-10-16 18:05:10 +0400

    Intial commit

---
+++ TerminalExtraction/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TerminalExtraction
+//
+//  Created by Yarik Smirnov on 10/16/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
