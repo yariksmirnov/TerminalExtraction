2e6cd035006d643b0d34726bfa4e4b7c664a2b31
Anton Katekov <void_main@iMac-Anton.local>
2012-12-07 16:38:06 +0400

    android

--- TerminalExtraction/os/android/jni/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <string.h>
-#include <jni.h>
-
-jstring Java_com_te_TerminalExtraction_TerminalExtraction_stringFromJNI( JNIEnv* env, jobject thiz )
-{
-    return (*env)->NewStringUTF(env, "My first JNI!");
-}
